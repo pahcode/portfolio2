36dcfb7b65fbd54b0df0fc225a28ab59a4289773
pahcode <wegurum004@gmail.com>
2024-12-23 22:47:09 +0900

    Add files via upload

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,21 @@
 $(window).on("load", function(){
   // 컴포넌트 로드
-  $('header').load('views/header.html');
-  $('main').load('views/home.html');
-  $('footer').load('views/footer.html');
+  $(".intro").load("views/intro.html");
+
+  setTimeout(function(){
+    console.log("test 입니다");
+    $("header").load("views/header.html");
+    $(".intro").stop().animate({
+      opacity: "0"
+      }, 100, function(){
+        this.remove();
+      }
+    );
+    $("main").load("views/home.html");
+    $("footer").load("views/footer.html");
+  }, 5250);
+
+
+  
 
 });
